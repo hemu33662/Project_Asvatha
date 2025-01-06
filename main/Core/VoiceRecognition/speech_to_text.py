0b5d5a13f492dff1720c3c3fa503380f7b25e262
hemanth n <hemanth.nitm@outlook.com>
2025-01-06 18:56:42 +0530

    sprint1

---
+++ main/Core/VoiceRecognition/speech_to_text.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+
+class ConvertSpeechToText:
+    def __init__(self, command_executor):
+        self.command_executor = command_executor
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+
+    def listen_and_recognize(self):
+        try:
+            with self.microphone as source:
+                print("Listening...")
+                audio = self.recognizer.listen(source, timeout=20)
+            return self.recognizer.recognize_google(audio)
+        except sr.WaitTimeoutError:
+            print("Timeout occurred while listening.")
+            return None
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            return None
+        # except KeyboardInterrupt:
+        #     print("Listening interrupted.")
+        #     return None
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            return None
+
+    
